dd68b2426705953036457ca1f28a66b995e2aece
Ali731-Amb <12252@holbertonstudents.com>
2026-01-05 14:28:53 +0100

    printenv

---
+++ 0-printenv.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - funtion thath print environnement
+ *
+ *Return: Environnement otherwise 0
+ */
+
+
+int main(void)
+{
+int i;
+
+while (environ[i] != NULL)
+{
+	printf("%s\n", environ[i]);
+	i++;
+}
+return (0);
+}
